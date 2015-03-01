8591b3f92f4fc75c9058b489e074825933618ba0
Jim Richardson <weaselkeeper@gmail.com>
2015-03-01 14:53:53 -0800

    minor delint

--- src/list-domains.py
+++ src/list-domains.py
@@ -4,6 +4,6 @@ import libvirt
 conn=libvirt.open("qemu:///session")
 
 for id in conn.listDomainsID():
-   dom = conn.lookupByID(id)
-   infos = dom.info()
-   print id, dom.name()
+    dom = conn.lookupByID(id)
+    infos = dom.info()
+    print id, dom.name()
