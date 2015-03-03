f89f47826503da6889020e7e3d86ad5f9bb0518a
Jim Richardson <weaselkeeper@gmail.com>
2015-03-03 11:04:09 -0800

    do not redifine builtins without a good reason

--- src/list-domains.py
+++ src/list-domains.py
@@ -3,7 +3,7 @@
 import libvirt
 conn = libvirt.open("qemu:///session")
 
-for id in conn.listDomainsID():
-    dom = conn.lookupByID(id)
+for dom_id in conn.listDomainsID():
+    dom = conn.lookupByID(dom_id)
     infos = dom.info()
-    print id, dom.name()
+    print dom_id, dom.name()
