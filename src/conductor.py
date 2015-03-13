79bfc2e4c468656972377e1459bbaa1db8ce99b3
Jim Richardson <weaselkeeper@gmail.com>
2015-03-12 19:44:26 -0700

    minor delint

--- src/conductor.py
+++ src/conductor.py
@@ -74,8 +74,8 @@ def get_options():
     parser.add_argument('-d', '--debug', action='store_true',
                         help='Enable debugging during execution.',
                         default=None)
-    parser.add_argument('-l', '--list_domains', action='store_true', default=False,
-                        help='List active domains')
+    parser.add_argument('-l', '--list_domains', action='store_true',
+                        default=False, help='List active domains')
     parser.add_argument('-c', '--config', action='store', default=None,
                         help='Specify a path to an alternate config file')
 
